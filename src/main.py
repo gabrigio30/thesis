a2474d159a260ce7cce637ae065bbbb8b7f04900
Gabriele Giordanelli <gab.giordanell@gmail.com>
2026-01-21 16:26:32 +0100

    Test

--- src/main.py
+++ src/main.py
@@ -13,11 +13,11 @@ from src.variant_generator2 import (
 )
 
 def main():
-    funcs = load_functions('SpectreV4Tests/spectre.s')
+    funcs = load_functions('SpectreV1Tests/spectreHPS.s')
     results = annotate_transient_instructions(funcs, window_size=7,
-                                              enabled_detectors = [#'detect_cmp_jcc_mem',
+                                              enabled_detectors = ['detect_cmp_jcc_mem',
                                                                    #'detect_indirect_branch',
-                                                                   'detect_store_then_load',
+                                                                   #'detect_store_then_load',
                                                                    ])
 
     for r in results:
@@ -32,25 +32,25 @@ def main():
     # Esempio: 20% nop, 30% fence, 40% lea_split, 10% reorder_movs
     transform_mix = {
         #transform_index_masking_light: 1,
-        transform_nop: 0.2,
-        transform_lea_split: 0.15,
+        #transform_nop: 0.2,
+        #transform_lea_split: 0.15,
         #transform_reorder_movs: 1,
-        #transform_fence_after_jcc: 1,
-        transform_fence_between_store_load: 0.5,
-        transform_ssb_dependency_chain_barrier: 0.15,
+        transform_fence_after_jcc: 1,
+        #transform_fence_between_store_load: 0.5,
+        #transform_ssb_dependency_chain_barrier: 0.15,
         #transform_retpoline_rewrite: 1,
     }
 
     out = generate_variants_for_results(
         funcs,
         results,
-        num_variants=2500,
+        num_variants=100,
         same_variants=False,
-        transforms_per_variant=6,   # N trasformations per variant
+        transforms_per_variant=1,   # N trasformations per variant
         transform_weights=transform_mix,
     )
 
-    write_functions(out, 'SpectreV4Tests/spectreAll2500.s')
+    write_functions(out, 'SpectreV1Tests/spectreHPSfence100.s')
 
 
 if __name__ == "__main__":
